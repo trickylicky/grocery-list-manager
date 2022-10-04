bcdaf9532c7301d5e061d713ad4e8fbfe6e40909
Tricky Licky <wangombevictor5@gmail.com>
2022-10-04 13:32:47 +0300

    create app and add title app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          GROCERY TRACK 
+        </h1>
+        <img src="https://images.pexels.com/photos/9706139/pexels-photo-9706139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="App-logo" alt="logo" />
+        
+       
+         
       </header>
     </div>
   );
