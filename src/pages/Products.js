ba64d48694e5840e0d788647c61ce3e0a34da57f
Tricky Licky <wangombevictor5@gmail.com>
2022-10-07 23:19:12 +0300

    Add food Price

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -40,7 +40,7 @@ const Products = ({url , newId})=>{
         <h1>SELECTED ITEMS WILL BE DISPLAYED BELOW :</h1><ul>
         {console.log(newId)}
         {newId === null ? "select product": <>
-        {newId.food}
+        {newId.food} - {newId.price}
         </>}
             </ul>
         </>
