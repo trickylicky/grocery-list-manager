773ab36c214ad6fa5565cf9f7da207a8ad52a859
Tricky Licky <wangombevictor5@gmail.com>
2022-10-08 14:44:20 +0300

    Add an about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,9 @@
 const About = ()=>{
     return (
         <>
-        About
+        <h1><strong>About This App</strong></h1>
+        <p>A high percentage of people while visiting the local store tend to us more than what the had budget even though the had a list of the products they needed to buy.
+        <strong>In this app you</strong> can easily check the price of products of your preffered choice .You can have a summary of what you want to buy and how much you will spend.The user can also add product and prices from their local store to the App which can be viewed by other users</p>
         </>
     )
 
